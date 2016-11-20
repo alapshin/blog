1c3bccbb54c3dd4f822a0227b6166dd8b5e95ec6
Andrei Lapshin <alapshin@fastmail.com>
2016-11-20 09:32:07 +0300

    Add font-awesome to project

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,49 +2,49 @@ var webpack = require('webpack');
 var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-
 var options = {
-  entry: {
-    'app': './js/main.js',
-    'styles': './scss/main.scss'
-  },
-  output: {
-    path: path.dirname(__dirname) + '/assets/static/gen',
-    filename: '[name].js'
-  },
-  devtool: '#cheap-module-source-map',
-  resolve: {
-    modulesDirectories: ['node_modules'],
-    extensions: ['', '.js']
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
-      },
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-      },
-      {
-        test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png|\.jpe?g\|\.gif$/,
-        loader: 'file'
-      }
+    entry: {
+        'app': './js/main.js',
+        'styles': './scss/main.scss'
+    },
+    output: {
+        path: path.dirname(__dirname) + '/assets/static/gen',
+        filename: '[name].js',
+        publicPath: '/static/gen/',
+    },
+    devtool: '#cheap-module-source-map',
+    resolve: {
+        extensions: ['', '.js'],
+        modulesDirectories: ['node_modules']
+    },
+    module: {
+        loaders: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        },
+        {
+            test: /\.(png|jpg|jpeg|gif)$/, 
+            loader: 'url?limit=10000&name=images/[name].[ext]'
+        },
+        {
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
+        },
+        {
+            test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+            loader: 'file-loader'
+        }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('styles.css', {
+            allChunks: true
+        }),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin()
     ]
-  },
-  plugins: [
-    new ExtractTextPlugin('styles.css', {
-      allChunks: true
-    }),
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.optimize.DedupePlugin()
-  ]
 };
 
 module.exports = options;
